311682c8f9f30ba519af28f6283504aceaebf4e8
yingde0117 <47807095+yingde0117@users.noreply.github.com>
2019-02-20 14:44:48 +0800

    Add files via upload

---
+++ incredible_blad_tumelo1.ino
@@ -0,0 +1,49 @@
+void setup()
+{
+  pinMode(9, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(9, HIGH);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(9, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(9, HIGH);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(9, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(9, HIGH);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(9, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(9, HIGH);
+  delay(300); // Wait for 1000 millisecond(s)
+  digitalWrite(9, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(9, HIGH);
+  delay(300); // Wait for 1000 millisecond(s)
+  digitalWrite(9, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(9, HIGH);
+  delay(300); // Wait for 1000 millisecond(s)
+  digitalWrite(9, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(9, HIGH);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(9, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(9, HIGH);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(9, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(9, HIGH);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(9, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(9, LOW);
+  delay(300); // Wait for 1000 millisecond(s)
+  
+}
